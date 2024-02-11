b4cd83fdd43b5d6b6b362705d5138ccf5e829cbc
emersooc <emersooc@mail.uc.edu>
2024-02-11 18:28:02 -0500

    Finished Task

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class TheaterKiosk
 {
     public static void main(String[] args)
     {
+        Scanner userInput = new Scanner(System.in); //creates scanner object
+        System.out.println("Please enter your age: ");
+        int age = userInput.nextInt(); //reads user input
 
+        //determines if they are 21 or older and acts accordingly
+        if (age >= 21)
+        {
+            System.out.println("Please take a wristband");
+        }
     }
 }
